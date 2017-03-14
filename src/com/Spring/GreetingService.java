89f0a79da7408fef39c08b6b24d3ab00f2c6ed2f
RAVITEJA\cb <dyaparaviteja@gmail.com>
2017-03-13 20:17:22 -0400

    added comments to the greeting service

--- src/com/Spring/GreetingService.java
+++ src/com/Spring/GreetingService.java
@@ -1,8 +1,12 @@
 package com.Spring;
 
+/**
+ * 
+ * @author RaviTeja Created unimplemented greeting service method to print
+ *         Greeting Text
+ *
+ */
 public interface GreetingService {
 	public void sayGreeting();
 	
-	
-
 }
